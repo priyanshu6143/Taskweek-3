e107da2f5a12d41f17f1937ee54f7ce3d79ce187
Priyanshu <priyanshu6143@gmail.com>
2026-09-24 13:28:18 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Scaffold(
+        body:Container(
+          child: Column(
+            children: [
+              for(int j = 0; j < 8 ; j++)
+                if(j % 2 == 0)...[
+                  Row(
+                    children: [
+                      for(int i = 0; i < 8 ; i++)
+                        if(i % 2 == 0)...[
+                          Container(
+                            height : MediaQuery.of(context).size.height / 9,
+                            width : MediaQuery.of(context).size.width / 9,
+                            color: Colors.white70,
+                          )
+                        ]
+                        else...[
+                          Container(
+                            height : MediaQuery.of(context).size.height / 9,
+                            width : MediaQuery.of(context).size.width / 9,
+                            color: Colors.black,
+                          ),
+                        ],
+                    ],
+                  ),
+                ]
+              else...[
+                  Row(
+                    children: [
+                      for(int i = 0; i < 8 ; i++)
+                        if(i % 2 == 0)...[
+                          Container(
+                            height : MediaQuery.of(context).size.height / 9,
+                            width : MediaQuery.of(context).size.width / 9,
+                            color: Colors.black,
+                          )
+                        ]
+                        else...[
+                          Container(
+                            height : MediaQuery.of(context).size.height / 9,
+                            width : MediaQuery.of(context).size.width / 9,
+                            color: Colors.white70,
+                          ),
+                        ],
+                    ],
+                  ),
+                ]
+            ],
+          ),
+        )
+        ),
+      );
+  }
+}
